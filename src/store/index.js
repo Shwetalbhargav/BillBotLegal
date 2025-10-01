1f0ab208134601042f4a89292ff125eac379635b
Shwetalbhargav <hridaan.purav@gmail.com>
2025-10-01 16:28:53 +0530

    upgraded user/register

--- src/store/index.js
+++ src/store/index.js
@@ -1,77 +1,36 @@
 // src/store/index.js
 import { configureStore } from '@reduxjs/toolkit';
 
-// Existing reducers
-import authReducer from './authSlice';
-import clientReducer from './clientSlice';
-import caseReducer from './caseSlice';
-import billableReducer from './billableSlice';
-import invoiceReducer from './invoiceSlice';
-import analyticsReducer from './analyticsSlice';
-import emailReducer from './emailSlice';
-import usersReducer from './usersSlice';
+import auth from './authSlice';
+import users from './usersSlice';
+import register from './registerSlice';
+import clients from './clientSlice';
+import cases from './caseSlice';
+import billables from './billableSlice';
+import invoices from './invoiceSlice';
+import analytics from './analyticsSlice';
+import emails from './emailSlice';
 
-// New: registration slice
-import registerReducer from './registerSlice';
+// Role profile slices (exact filenames)
+import partnerProfile from './partnerProfileSlice';
+import lawyerProfile from './lawyerProfileSlice';
+import associateProfile from './associateProfileSlice';
+import internProfile from './internProfileSlice';
 
-// New: role profile slices
-import partnerProfileReducer, { partnerProfileThunks } from './partnerProfileSlice';
-import lawyerProfileReducer, { lawyerProfileThunks } from './lawyerProfileSlice';
-import associateProfileReducer, { associateProfileThunks } from './associateProfileSlice';
-import internProfileReducer, { internProfileThunks } from './internProfileSlice';
-
-const store = configureStore({
+export default configureStore({
   reducer: {
-    // existing
-    users: usersReducer,
-    auth: authReducer,
-    clients: clientReducer,
-    cases: caseReducer,
-    billables: billableReducer,
-    invoices: invoiceReducer,
-    analytics: analyticsReducer,
-    emails: emailReducer,
-
-    // new
-    register: registerReducer,
-    partnerProfile: partnerProfileReducer,
-    lawyerProfile: lawyerProfileReducer,
-    associateProfile: associateProfileReducer,
-    internProfile: internProfileReducer,
+    auth,
+    users,
+    register,
+    clients,
+    cases,
+    billables,
+    invoices,
+    analytics,
+    emails,
+    partnerProfile,
+    lawyerProfile,
+    associateProfile,
+    internProfile,
   },
 });
-
-export default store;
-
-// Optional: central re-exports of the most-used thunks
-export const {
-  fetchMine: fetchPartnerProfileMine,
-  fetchByUser: fetchPartnerProfileByUser,
-  createProfile: createPartnerProfile,
-  updateProfile: updatePartnerProfile,
-  removeProfile: removePartnerProfile,
-} = partnerProfileThunks;
-
-export const {
-  fetchMine: fetchLawyerProfileMine,
-  fetchByUser: fetchLawyerProfileByUser,
-  createProfile: createLawyerProfile,
-  updateProfile: updateLawyerProfile,
-  removeProfile: removeLawyerProfile,
-} = lawyerProfileThunks;
-
-export const {
-  fetchMine: fetchAssociateProfileMine,
-  fetchByUser: fetchAssociateProfileByUser,
-  createProfile: createAssociateProfile,
-  updateProfile: updateAssociateProfile,
-  removeProfile: removeAssociateProfile,
-} = associateProfileThunks;
-
-export const {
-  fetchMine: fetchInternProfileMine,
-  fetchByUser: fetchInternProfileByUser,
-  createProfile: createInternProfile,
-  updateProfile: updateInternProfile,
-  removeProfile: removeInternProfile,
-} = internProfileThunks;
