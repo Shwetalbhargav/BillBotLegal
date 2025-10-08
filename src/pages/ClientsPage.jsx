eaebc68a22c3afa7625eeb4006e38c1123fe96b7
Shwetalbhargav <hridaan.purav@gmail.com>
2025-10-08 19:56:43 +0530

    removed import error

--- src/pages/ClientsPage.jsx
+++ src/pages/ClientsPage.jsx
@@ -1,6 +1,7 @@
 // src/pages/ClientsPage.jsx
 import React from 'react';
-import ClientDashboard from '@/features/shared/ClientsDashboardBase';
+import ClientDashboard from '@/features/shared/CaseDashboardBase';
+
 
 const ClientsPage = () => <ClientDashboard />;
 
