7f8dd82d60fd64fc09a231bfe800eac40104c1af
Shwetalbhargav <hridaan.purav@gmail.com>
2025-12-10 14:30:46 +0530

    added images

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -3,8 +3,6 @@ export { default as Pagination } from "./Pagination";
 export { default as SkeletonRows } from "./SkeletonRows";
 export { default as Column } from "./Column";
 export { default as TableToolbar } from "./TableToolbar";
-export {default as CaseTable} from "./CaseTable";
-export {default as ClientTable} from "./ClientTable";
 export {default as EmptyState} from "./EmptyState";
 export {
   Table,
